a3d712eb3bc14c9807c9c4c0e207fddc5b37e181
Summer Duke <esrduke@gmail.com>
2020-10-31 08:28:34 -0500

    progress on READ

--- src/javascripts/components/views/allBoardsView.js
+++ src/javascripts/components/views/allBoardsView.js
@@ -1,15 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import card from '../cards/boardCards';
 import boardData from '../../helpers/data/boardData';
 
 const boardsView = () => {
-  boardData.getAllBoards().then((response) => {
-    if (response.length) {
-      response.forEach((item) => {
-        $('#app').append(card.boardBuilder(item));
-      });
-    } else {
-      $('#app').append('<h2>NO BOARDS</h2>');
-    }
+  firebase.auth().onAuthStateChanged((user) => {
+    boardData.getUserBoards(user.uid).then((response) => {
+      if (response.length) {
+        response.forEach((item) => {
+          $('#app').append(card.boardBuilder(item));
+        });
+      } else {
+        $('#app').append('<h2>NO BOARDS</h2>');
+      }
+    });
   });
 };
 
--- src/javascripts/helpers/data/boardData.js
+++ src/javascripts/helpers/data/boardData.js
@@ -22,7 +22,6 @@ const getAllBoards = () => new Promise((resolve, reject) => {
 const getUserBoards = (userUid) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/boards.json?orderBy="userUid"&equalTo="${userUid}"`)
     .then((response) => {
-      console.warn(response);
       const usersBoards = response.data;
       const boards = [];
       if (usersBoards) {
