7a1ff39e29def2015f35ec3bc270621fcf5c52f7
Summer Duke <esrduke@gmail.com>
2020-11-07 07:46:51 -0600

    fixes

--- src/javascripts/components/nav/nav.js
+++ src/javascripts/components/nav/nav.js
@@ -11,7 +11,6 @@ const logoutEvent = () => {
 };
 
 const navbar = (currentUser) => {
-  console.warn('works');
   $('#nav').html(`<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <a class="navbar-brand" href="#" id="site-title">finterest</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,20 +19,20 @@ const navbar = (currentUser) => {
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" id="home-link" href="#">Home <span class="sr-only"></span></a>
+          <a class="nav-link" id="home-link" href="#">home<span class="sr-only"></span></a>
         </li>
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           my boards
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">board 1</a>
+          <a class="dropdown-item" id="board1" href="#">board 1</a>
           <a class="dropdown-item" href="#">board 2</a>
           <a class="dropdown-item" href="#">board 3</a>
         </div>
       </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">add pin +</a>
+          <a class="nav-link" id="add-pin-link" href="#">add pin +</a>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
